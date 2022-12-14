dceee33644513b88f66c077c396ea7ced5c3d280
sugyant <sugyantchettri@gmail.com>
2022-12-15 00:34:27 +0800

    db password default

--- src/db/tenantConfigDB.ts
+++ src/db/tenantConfigDB.ts
@@ -5,7 +5,7 @@ const dbConn = new Sequelize({
 	dialect: "mysql",
 	host:"localhost",
 	username:"root",
-	password:"Aprisha09",
+	password:"YourPassword",
 	database:"tenant_configs",
 	logging: false,
 	models:[Configs]
